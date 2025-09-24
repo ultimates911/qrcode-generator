1c3be89b6e596323f14411fc7055982cab72180a
Кирилл Принзин <kprinzin@anykey.pro>
2025-09-24 17:06:25 +0300

    feat(arch): made the project runnable, added initial arch files

--- backend/cmd/server/main.go
+++ backend/cmd/server/main.go
@@ -1,25 +1,14 @@
 package main
 
 import (
-	"encoding/json"
-	"log"
-	"net/http"
 	"os"
+	"time"
+
+	"github.com/rs/zerolog"
+	"github.com/rs/zerolog/log"
 )
 
+
 func main() {
-	mux := http.NewServeMux()
-
-	mux.HandleFunc("/api/hello", func(w http.ResponseWriter, r *http.Request) {
-		type resp struct{ Message string `json:"message"` }
-		_ = json.NewEncoder(w).Encode(resp{Message: "привет из Go"})
-	})
-
-	port := os.Getenv("PORT")
-	if port == "" {
-		port = "8080"
-	}
-	addr := ":" + port
-	log.Println("listening on", addr)
-	log.Fatal(http.ListenAndServe(addr, mux))
+	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr, TimeFormat: time.RFC3339})
 }
